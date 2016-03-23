9bfe422cf0045a2f966d90975e202e0f924e3d24
Radziu <radek.k97@gmail.com>
2016-03-23 23:51:39 +0100

    drobny fix, ale nie działa jeszcze

--- src/AppBundle/Controller/TeacherSessionController.php
+++ src/AppBundle/Controller/TeacherSessionController.php
@@ -25,9 +25,11 @@ class TeacherSessionController extends Controller
         $em = $this->getDoctrine()->getManager();
         $sessions_all = $em->getRepository("AppBundle:QuizSession");
         $qb = $sessions_all->createQueryBuilder('s');
-        $datetime_now = new \DateTime(date('Y-m-d H:i:s'));
+        $datetime_now = new \DateTime(date('Y-m-d H:i:s')); <--nie pyka
+        $datetime_now = Date('Y-m-d');
+        
         $sessions = $qb
-            ->where('s.end'<$datetime_now)->getQuery()->getResult();
+            ->where('s.end<'.$datetime_now)->getQuery()->getResult();
 
 
         return $this->render('teacher/active_sessions.html.twig', array(
