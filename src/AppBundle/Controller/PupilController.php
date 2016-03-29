baf914b49e603554092205ed7e38b039e4bdbb64
Radziu <radek.k97@gmail.com>
2016-03-29 19:13:14 +0200

    zmiany w navbarze pupila, widok indexu pupila, początek widoku pupil_view_attempt. Bowlin' time

--- src/AppBundle/Controller/PupilController.php
+++ src/AppBundle/Controller/PupilController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Attempt;
+use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,8 +15,13 @@ class PupilController extends Controller{
      */
     public function pupilIndexAction()
     {
-
-        return $this->render('pupil/view_index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $attempts = $em->getRepository('AppBundle:Attempt')->findByUser($this->getUser());
+        $results = $em->getRepository('AppBundle:Result')->findAll();
+        return $this->render('pupil/view_index.html.twig', array(
+            'attempts' => $attempts,
+            'results' => $results,
+        ));
     }
 
     public function pupilSidebarAction()
@@ -23,6 +30,22 @@ class PupilController extends Controller{
         $user = $em->getRepository('AppBundle:User')->find($this->getUser());
         return $this->render('pupil/sidebar.html.twig', array(
             'user'=>$user,
+        ));
+    }
+
+    /**
+     * @Route("/pupil/view/attempt/{attempt}/", name="pupil_view_attempt")
+     */
+    public function pupilViewAttemptAction($attempt)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $attempt = $em->getRepository('AppBundle:Attempt')->find($attempt);
+        $result = $em->getRepository('AppBundle:Result')->find($attempt);
+        $user_answers = $em->getRepository('AppBundle:UserAnswer')->findByAttempt($attempt);
+        return $this->render('pupil/view_attempt.html.twig', array(
+            'attempt'=>$attempt,
+            'result' => $result,
+            'user_answers'=>$user_answers,
         ));
     }
 }
