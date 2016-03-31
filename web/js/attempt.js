61538e1b25f71c4dd337ec93e7c0c97dbd99c1fd
Radziu <radek.k97@gmail.com>
2016-03-31 22:50:15 +0200

    prace nad AttemptController, coś nie pyka ale ide spać

--- web/js/attempt.js
+++ web/js/attempt.js
@@ -1,8 +1,11 @@
 //angular.module('myApp', []).controller('myCtrl', function($scope, $http, $interval) {
 angular.module('myApp', []).controller('attempt', function ($scope, $http, $interval){
 
+
     $scope.setAttempt = function(attempt){
+        //var attempt = attempt;
         $scope.attempt = attempt;
+        //attempt = attempt;
     };
 
     //tutaj defaultowe wartości
@@ -10,23 +13,26 @@ angular.module('myApp', []).controller('attempt', function ($scope, $http, $inte
     $scope.question = "Choose a question from the list on the right";
          //end defaultowych wartości
     $scope.getQuestion = function(question){
-        $http.post('/attempt/ajax/question', question).then(function(response){
-            $scope.answers = response.data.answers;
-            $scope.user_answers = response.data.user_answers;
-        },function(response){
-            alert("Wystąpił błąd.");
-        });
+        //$http.post('/attempt/ajax/question', question).then(function(response){
+        //    $scope.answers = response.data.answers;
+        //    $scope.user_answers = response.data.user_answers;
+        //},function(response){
+        //    alert("Wystąpił błąd.");
+        //});
+        
     };
 
     getQuestions = function(attempt){
-      $http.post('/attempt/ajax/attempt', 1).then(function(response){
-          $scope.attempt = response.data.attempt;
-          $scope.answers = response.data.answers;
-          $scope.result =  response.data.result;
+      $http.post('/attempt/ajax/attempt/', $scope.attempt).then(function(response){
+          //$scope.attempt = response.data.attempt
+          //$scope.answers = response.data.answers;
+          //$scope.result =  response.data.result;
           $scope.user_answers = response.data.user_answers;
       },function(response){
           alert("Wystąpił błąd.");
       });
     };
-    getQuestions(1);
+
+    getQuestions($scope.attempt);
+    $interval(getQuestions, 2500);
 });
