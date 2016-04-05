ddb958cd2a1386ed3c1aad95b970f06cb7d589d9
Radziu <radek.k97@gmail.com>
2016-04-05 09:00:00 +0200

    imie i nazwisko

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -25,6 +25,16 @@ class User extends BaseUser
      */
     protected $grade;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $firstname;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $lastname;
+
     public function __construct()
     {
         parent::__construct();
@@ -52,5 +62,51 @@ class User extends BaseUser
     public function getGrade()
     {
         return $this->grade;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string 
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string 
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
     }
 }
